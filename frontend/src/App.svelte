27fa9c78617bb73f08478fdbf6223b411ac7d879
Your Name <you@example.com>
2021-02-01 02:15:39 -0500

    Second  commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+import ShowResults from "./ShowResults.svelte";
+$:birthDate = ''
+let birthDateHeader = '';
+$: show = false
+const showEvents =(e)=>{
+	show = true
+	birthDateHeader = new Date(birthDate).toDateString()
+	
+}
+</script>
+<svelte:head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<title>YO</title>
+</svelte:head>
+<main>
+	<div class="container">
+		<h3 class='text-center'>Yo</h3>
+		<div class="container">
+			<div class="input-group">
+				<label class='m-4' for=""> 
+					{birthDateHeader ? `Your birthday ${birthDateHeader}`:'Enter your birthday'} 
+				</label>
+				<input bind:value={birthDate} type="date">
+				<button on:click={showEvents} class='btn btn-primary'>See</button>
+			</div>
+			<div class="container">
+				{#if show}
+					<ShowResults birthDate={birthDate}/>
+				{/if}
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	.input-group{
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: space-evenly;
+	}
+</style>
---
+++ frontend/src/ShowResults.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let birthDate;
+    import { numberWithCommas } from "./utils";
+    const usersBDate = new Date(birthDate).getTime();
+    let billionthSecond = new Date(usersBDate + 1000000000 * 1000);
+    $:currentSecond = ''
+    setInterval(()=>{
+        currentSecond = numberWithCommas(parseInt(((new Date(Date.now() - usersBDate).getTime())/1000)))
+    },1000)
+
+</script>
+
+<section>
+    <ul class="list-group mt-3">
+        <li class="list-group-item">
+            The day on which you live your billionth second is
+            {billionthSecond.toString()}
+        </li>
+        <li class="list-group-item">
+            You are on {currentSecond}th second right now(approx.)
+        </li>
+    </ul>
+</section>
+
+<style>
+</style>
